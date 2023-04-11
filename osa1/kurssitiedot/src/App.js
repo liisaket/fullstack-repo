eab15c88f631e9b38997ec55c67c0b2567329289
liisa <liisa-m@outlook.com>
2023-04-11 17:46:22 +0300

    2.5 erillinen moduuli

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,37 +1,4 @@
-const Header = ({ course }) => {
-  return <h2>{course}</h2>
-}
-
-const Total = ({ parts }) => {
-  const total = parts.map(part => part.exercises).reduce((s, p) => s + p, 0)
-  return <b>total of {total} exercises</b>
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
-        )}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
