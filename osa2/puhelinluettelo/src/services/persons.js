45cca8760342fcc6c4862619193015c27f27dae8
liisa <liisa-m@outlook.com>
2023-04-20 20:41:46 +0300

    2.15* puhelinluettelo step10

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -18,4 +18,9 @@ const del = (id) => {
     return request.then(response => response.data)
 }
 
-export default { getAll, create, del }
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+  }
+
+export default { getAll, create, del, update }
