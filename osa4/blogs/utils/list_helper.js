ba728f7075b41bd2b469e074e1a5db316b68fd56
liisa <liisa-m@outlook.com>
2023-10-10 16:24:35 +0300

    4.4 tests step2

--- osa4/blogs/utils/list_helper.js
+++ osa4/blogs/utils/list_helper.js
@@ -1,7 +1,15 @@
+const logger = require('../utils/logger')
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  return likes.reduce((a, b) => a + b, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
