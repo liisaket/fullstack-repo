2eb0b367e6bb1689c7792bf15deb825a60fd79ed
liisa <liisa-m@outlook.com>
2023-10-17 18:49:01 +0300

    4.15 bloglist step3

--- osa4/blogs/controllers/routes.js
+++ osa4/blogs/controllers/routes.js
@@ -1,11 +1,11 @@
+const jwt = require('jsonwebtoken')
 const theRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-// Muistiinpanon luominen
-
 theRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -18,17 +18,35 @@ theRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 theRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  
   response.status(201).json(savedBlog)
 })
 
--- osa4/blogs/tests/users.test.js
+++ osa4/blogs/tests/users.test.js
@@ -20,9 +20,9 @@ describe('when there is initially one user at db', () => {
     const usersAtStart = await helper.usersInDb()
 
     const newUser = {
-      username: 'mluukkai',
-      name: 'Matti Luukkainen',
-      password: 'salainen',
+      username: 'testuser',
+      name: 'Super Mario',
+      password: 'secret',
     }
 
     await api
@@ -44,7 +44,7 @@ describe('when there is initially one user at db', () => {
     const newUser = {
       username: 'root',
       name: 'Superuser',
-      password: 'salainen',
+      password: 'secret',
     }
 
     const result = await api
@@ -54,6 +54,29 @@ describe('when there is initially one user at db', () => {
       .expect('Content-Type', /application\/json/)
 
     expect(result.body.error).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test('creation fails if password length is less than 3', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mario',
+      name: 'Mario',
+      password: 'ma',
+    }
+
+    const result = await api
+    .post('/api/users')
+    .send(newUser)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain(
+      'User validation failed: `password` is shorter than the minimum allowed length (3).'
+    )
 
     const usersAtEnd = await helper.usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length)
