33214cc83b1a2b241c6d06e3a0c1ae56145c3109
liisa <liisa-m@outlook.com>
2023-11-27 16:42:28 +0200

    getting data w react query works

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,3 +1,7 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
+
+
 import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
@@ -8,18 +12,38 @@ import { initializeAnecdotes, setAnecs } from './reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(initializeAnecdotes())
-   }, [])
+  
+  const queryClient = useQueryClient()
+
+  const result = useQuery({
+  queryKey: ['anecdotes'],
+  queryFn: getAnecdotes
+  })
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Notification />
-      <Filter />
-      <AnecdoteList />
-      <AnecdoteForm />
+
+      {[...anecdotes].sort(function(first, second) {
+        return second.votes - first.votes}).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id, anecdote)}>vote</button>
+          </div>
+          <br></br>
+        </div>
+      )
+      }
     </div>
   )
 }
--- osa6/redux-anecdotes/src/main.jsx
+++ osa6/redux-anecdotes/src/main.jsx
@@ -1,10 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
-import store from './store'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </Provider>
+  </QueryClientProvider>
 )
