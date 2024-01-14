eb76d952f169d60e3326307e9f70a0ef3e0018b7
liisa <liisa-m@outlook.com>
2024-01-14 20:43:44 +0200

    working on 7.14

--- osa7/blogapp/frontend/src/App.js
+++ osa7/blogapp/frontend/src/App.js
@@ -1,22 +1,32 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
 import { getUser } from './reducers/userReducer'
 import { initializeBlogs } from './reducers/blogReducer'
 
 import blogService from './services/blogs'
+import userService from './services/users'
 
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import Logout from './components/Logout'
 import BlogForm from './components/BlogForm'
 import BlogList from './components/BlogList'
+import Users from './components/Users'
 
 
 const App = () => {
   const dispatch = useDispatch()
   const user = useSelector(state => {
     return state.user})
+
+  const padding = {
+    paddingRight: 5
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs => {
@@ -28,18 +38,29 @@ const App = () => {
     dispatch(getUser())
   }, [dispatch])
 
+
   return (
-    <div>
-      <h2>blogs</h2>
-      <Notification />
-      <LoginForm />
-      {user && <div>
-        <Logout />
-        <BlogForm />
-        <BlogList />
+    <Router>
+      <div>
+        <h2>blogs</h2>
+        <Notification />
+        <LoginForm />
+        {user && <div>
+          <Link style={padding} to="/">home</Link>
+          <Link style={padding} to="/users">users</Link>
+          <Logout />
+          <Routes>
+            <Route path="/" element={
+              <div>
+                <BlogForm />
+                <BlogList />
+              </div>}/>
+            <Route path="/users" element={<Users />} />
+          </Routes>
+        </div>
+        }
       </div>
-      }
-    </div>
+    </Router>
   )
 }
 
