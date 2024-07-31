df3a9298521b4c25c02543883f6356c419288e56
liisa <liisa-m@outlook.com>
2024-07-31 20:30:17 +0300

    9.24

--- osa9/patientor/frontend/src/components/PatientInfo.tsx
+++ osa9/patientor/frontend/src/components/PatientInfo.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Diagnosis, Entry, Gender, Patient } from "../types";
 import patientService from "../services/patients";
+import diagnosesService from "../services/diagnoses";
 import { useEffect, useState } from "react";
 import Female from "@mui/icons-material/Female";
 import Male from "@mui/icons-material/Male";
@@ -8,6 +9,7 @@ import Male from "@mui/icons-material/Male";
 const PatientInfo = () => {
   const [patient, setPatient] = useState<Patient | undefined>();
   const { id } = useParams<{ id: string }>();
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
 
   useEffect(() => {
     const fetchPatient = async () => {
@@ -20,6 +22,18 @@ const PatientInfo = () => {
     };
     void fetchPatient();
   }, [id]);
+
+  useEffect(() => {
+    const fetchDiagnoses = async () => {
+      try {
+        const data = await diagnosesService.getAll();
+        setDiagnoses(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    void fetchDiagnoses();
+  }, []);
 
   return (
     <div>
@@ -40,9 +54,14 @@ const PatientInfo = () => {
               </p>
               {entry.diagnosisCodes && entry.diagnosisCodes.length > 0 && (
                 <ul>
-                  {entry.diagnosisCodes.map((code) => (
-                    <li key={code}>{code}</li>
-                  ))}
+                  {entry.diagnosisCodes.map((code) => {
+                    const diagnosis = diagnoses?.find((d) => d.code === code);
+                    return (
+                      <li key={code}>
+                        {code} {diagnosis ? diagnosis.name : ""}
+                      </li>
+                    );
+                  })}
                 </ul>
               )}
             </div>
